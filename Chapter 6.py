61a675cf4a5492ccc8475676a9495815d41212c5
arslanhiraj <143946675+arslanhiraj@users.noreply.github.com>
2023-09-13 23:06:05 +0500

    Add files via upload

---
+++ Chapter 6.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+rawstr = input('Enter a number:') 
+ival = int(rawstr)
+
+try:
+ ival = -1
+except:
+
+
+
+ if ival > 0: print('Nice work')
+
+else: print('Not a number')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
